97bafb89f2a1cc2f699b7dfc4ce3a77c4d78a1f5
IrvinS <irvinsegarciaflores@gmail.com>
2017-11-30 23:24:38 +0000

    Bug fix

--- src/config/config.php
+++ src/config/config.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'SMS_ALTIRIA_PROD_DOMAIN_ID' => 'ahorrolibre',
-    'SMS_ALTIRIA_PROD_LOGIN' => 'roberto@ahorrolibre.com',
-    'SMS_ALTIRIA_PROD_PASSWORD' => '7ALt2nXsKhz9'
+    env('SMS_ALTIRIA_PROD_DOMAIN_ID', 'ahorrolibre'),
+    env('SMS_ALTIRIA_PROD_LOGIN', 'roberto@ahorrolibre.com'),
+    env('SMS_ALTIRIA_PROD_PASSWORD', '7ALt2nXsKhz9')
 
 ];
